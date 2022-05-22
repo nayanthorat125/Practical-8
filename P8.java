33119e9b1e4b69a743d8240db01cd257dd9fb353
nayanthorat125 <66359304+nayanthorat125@users.noreply.github.com>
2022-05-22 22:30:32 +0530

    Create P8.java

---
+++ P8.java
@@ -0,0 +1,7 @@
+public class P6 {
+    public static void main(String[] args) { 
+        System.out.println("Hello Nayan Thorat");
+        System.out.println("This is your Third Practical of Jenkins");
+    }
+}
+
